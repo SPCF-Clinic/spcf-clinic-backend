25f4c2d935eae16b75fa9c525fb24346f784aea8
meliodafuuu318 <156494654+meliodafuuu318@users.noreply.github.com>
2026-09-07 12:52:32 +0800

    Return form field is_default value

--- app/Http/Resources/PersonalInfoFieldResource.php
+++ app/Http/Resources/PersonalInfoFieldResource.php
@@ -58,6 +58,7 @@ class PersonalInfoFieldResource extends JsonResource
         return [
             'id' => $this->id,
             'version_number' => $latestVersion?->version_number,
+            'is_default' => (bool) $this->is_default,
             'name' => $latestVersion?->field_name ?? $this->name,
             'type' => $type,
             'form_order' => $latestVersion?->form_order,
