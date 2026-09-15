bc8a97ef4286be128ae99b6da9e5e38f848d1eae
meliodafuuu318 <156494654+meliodafuuu318@users.noreply.github.com>
2026-09-15 16:41:53 +0800

    Return timer_paused_at field

--- app/Http/Resources/BedResource.php
+++ app/Http/Resources/BedResource.php
@@ -20,6 +20,7 @@ class BedResource extends JsonResource
             'bed_number' => $this->bed_number,
             'status' => $this->status,
             'timer_expires_at' => $this->timer_expires_at ? Carbon::parse($this->timer_expires_at)->format('Y-m-d H:i:s') : null,
+            'timer_paused_at' => $this->timer_paused_at ? Carbon::parse($this->timer_paused_at)->format('Y-m-d H:i:s') : null,
             'current_occupant' => $this->currentCheckIn ? [
                 'user_id' => $this->currentCheckIn->user_id,
                 'occupant_name' => $this->currentCheckIn->user->getStandardNameAttribute(),
